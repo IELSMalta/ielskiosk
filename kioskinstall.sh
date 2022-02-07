f81e0899713b6ef699a6f82ba149d6b1892610fd
IELSMalta <99180908+IELSMalta@users.noreply.github.com>
2022-02-07 13:23:11 +0100

    Update kioskinstall.sh

--- kioskinstall.sh
+++ kioskinstall.sh
@@ -5,12 +5,31 @@ apt upgrade
 echo '################## install software and depend #########'
 apt install freerdp2-x11 libavcodec-dev libasound2-dev libpulse-dev libxv-dev git xdotool -y
 echo '################## some ubuntu magic ####################'
+
+echo '################## black desktop wallpaper  ####################'
+
 gsettings set org.gnome.desktop.background picture-uri ""
 gsettings set org.gnome.desktop.background primary-color '#000000'
+
+echo '################## disable power sospension and blaking screen  ####################'
+
 xset s noblank
 xset s off
 xset -dpms
+gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'suspend'
+
+echo '################## permissions to sys user vdi ####################'
+
 usermod -aG sudo vdi
 usermod -aG plugdev vdi
 echo 'SUBSYSTEM=="usb", MODE="0660", GROUP="plugdev"' > /etc/udev/rules.d/00-usb-permissions.rules
 udevadm control --reload-rules
+
+mkdir /etc/ielskiosk
+mv ./kiosk.sh /etc/ielskiosk/kiosk.sh
+mv ./kiosk.service /lib/systemd/system/kiosk.service
+sudo systemctl enable kiosk.service
+sudo systemctl start kiosk.service
