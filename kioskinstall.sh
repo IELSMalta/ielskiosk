db8a6dfdbe676959495a84da882e8d23d83a569c
IELSMalta <99180908+IELSMalta@users.noreply.github.com>
2022-02-07 13:57:27 +0100

    Update kioskinstall.sh

--- kioskinstall.sh
+++ kioskinstall.sh
@@ -21,6 +21,25 @@ gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'suspend'
 
+echo '############ create system user #######'
+
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username vdi : " username
+	read -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p "$pass" "$username"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
+  
+fi
+
 echo '################## permissions to sys user vdi ####################'
 
 usermod -aG sudo vdi
