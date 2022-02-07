ff12d27205455758d466ed755979a0c1b22d2a55
IELSMalta <99180908+IELSMalta@users.noreply.github.com>
2022-02-07 13:11:05 +0100

    Update kioskinstall.sh

--- kioskinstall.sh
+++ kioskinstall.sh
@@ -10,23 +10,7 @@ gsettings set org.gnome.desktop.background primary-color '#000000'
 xset s noblank
 xset s off
 xset -dpms
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system."
-	exit 2
-fi
-usermod -aG sudo $username
-usermod -aG plugdev $username
+usermod -aG sudo vdi
+usermod -aG plugdev vdi
 echo 'SUBSYSTEM=="usb", MODE="0660", GROUP="plugdev"' > /etc/udev/rules.d/00-usb-permissions.rules
 udevadm control --reload-rules
