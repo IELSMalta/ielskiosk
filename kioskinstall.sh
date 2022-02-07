ce4f6670a6cd8d5532724975c3ef30b102f4ee2b
IELSMalta <99180908+IELSMalta@users.noreply.github.com>
2022-02-07 14:39:11 +0100

    Update kioskinstall.sh

--- kioskinstall.sh
+++ kioskinstall.sh
@@ -21,24 +21,24 @@ gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'suspend'
 
-echo '############ create system user #######'
+#echo '############ create system user #######'
 
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username (better use vdi as username): " username
-	read -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system."
-	exit 2
-fi
+#if [ $(id -u) -eq 0 ]; then
+#	read -p "Enter username (better use vdi as username): " username
+#	read -p "Enter password : " password
+#	egrep "^$username" /etc/passwd >/dev/null
+#	if [ $? -eq 0 ]; then
+#		echo "$username exists!"
+#		exit 1
+#	else
+#		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+#		useradd -m -p "$pass" "$username"
+#		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+#	fi
+#else
+#	echo "Only root may add a user to the system."
+#	exit 2
+#fi
 
 echo '################## permissions to sys user vdi ####################'
 
