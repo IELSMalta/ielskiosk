fae2b98e4df782f71183f911c5032907ade482f4
IELSMalta <99180908+IELSMalta@users.noreply.github.com>
2022-02-07 14:27:45 +0100

    Update kioskconfig.sh

--- kioskconfig.sh
+++ kioskconfig.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+sudo systemctl stop kiosk.service
 read -p "Enter remote system name: " remote
 echo "Remote server saved!"
+#sudo systemctl enable kiosk.service
+sudo systemctl start kiosk.service
