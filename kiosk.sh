5707c1ae0511696004fdec1ab85dd37e6a8dc8d0
IELSMalta <99180908+IELSMalta@users.noreply.github.com>
2022-03-16 10:16:53 +0100

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,6 +1,13 @@
 ##!/bin/bash
 
+server=
+serverbkp=
+sleep 10
 while true
-do xfreerdp /v:$remote /u:$user -f /cert-ignore /sec:tls /vc:rdpsnd,sys:alsa  +gfx-thin-client /usb:dev:046d:0825
+do
+echo connecting to VDI
+  xfreerdp /v:$server /u:$user -f /cert-ignore /sec:tls /vc:rdpsnd,sys:alsa  +gfx-thin-client /rfx /rfx-mode:video /rfx-mode:audio /gfx:RFX +glyph-cache -toggle-fullscreen /multimon:force
+echo connecting to VDI
+  xfreerdp /v:$serverbkp /u:$user -f /cert-ignore /sec:tls /vc:rdpsnd,sys:alsa  +gfx-thin-client /rfx /rfx-mode:video /rfx-mode:audio /gfx:RFX +glyph-cache -toggle-fullscreen /multimon:force
 sleep 1
 done
